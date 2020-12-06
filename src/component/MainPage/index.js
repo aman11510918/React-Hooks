22b2a4bb0b3061ece25d303fcb315ebe95675f10
Aman Pratap Singh <aman.singh@kuliza.com>
2020-12-06 17:58:50 +0530

    used useEffect hook

--- src/component/MainPage/index.js
+++ src/component/MainPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 export const Main = () => {
@@ -11,7 +11,18 @@ export const Main = () => {
   /*
   // setting up state variable using useState and value
   const [count, setCount] = useState(0);
+  
+  // Read more on useState use
+  // https://reactjs.org/docshooks-overviewhtml#declaring-multiple-state-variables
   */
+
+  // setting up useEffect hook - Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    console.log("Inside use effect");
+    return () => {
+      console.log("inside useEffect cleanUp section i.e return function");
+    };
+  });
 
   // handling increase count button click function
   const onHandleIncrease = () => {
@@ -26,7 +37,8 @@ export const Main = () => {
   return (
     <React.Fragment>
       <h1>Hello, You're seeing me from MainPage functional component</h1>
-      <h1 className="countingBox">{count}</h1>
+      <h1 className="countingBox"> {count} </h1>
+      <br />
       <button className="button" onClick={() => onHandleIncrease()}>
         Increase Count
       </button>
